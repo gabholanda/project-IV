511ad90093137a021d1be2128030331389d180ce
GABRIEL HOLANDA DOS SANTOS <gabriel.hsantos21@senacsp.edu.br>
2020-03-03 21:51:00 -0300

    java 7 merda

--- projectpiiv/src/main/java/br/senac/sp/grupoum/projectpiiv/controllers/ProdutoServlet.java
+++ projectpiiv/src/main/java/br/senac/sp/grupoum/projectpiiv/controllers/ProdutoServlet.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
- 
+
 import javax.imageio.ImageIO;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -20,67 +20,71 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import br.senac.sp.grupoum.projectpiiv.models.Produto;
+import com.oreilly.servlet.MultipartRequest;
 import dao.ProdutoDAO;
-
-
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import org.apache.commons.fileupload.FileItemFactory;
 
 @WebServlet(name = "ProdutoServlet", urlPatterns = {"/cadastrar-produto"})
-
 public class ProdutoServlet extends HttpServlet {
 
-	
-	
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/cadastrar-produto.jsp");
-   		 	dispatcher.forward(request, response);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/cadastrar-produto.jsp");
+        dispatcher.forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	
-    	/*Identifica se o formulario é do tipo multipart/form-data*/
+        /*Identifica se o formulario é do tipo multipart/form-data*/
+        MultipartRequest m = new MultipartRequest(request, "C:/Users/gabriel.hsantos21/Desktop/teste");
+        String nome = m.getParameter("nome");
+        String descricao = m.getParameter("descricao");
+        String tipo = m.getParameter("tipo");
+        double preco = Double.valueOf(m.getParameter("preco"));
+        double quantidade = Double.valueOf(m.getParameter("quantidade"));
         if (ServletFileUpload.isMultipartContent(request)) {
-            try {request.setCharacterEncoding("UTF-8");
- 
-    		String nome = request.getParameter("nome");
-    		String descricao = request.getParameter("descricao");
-    		String tipo = request.getParameter("tipo");
-    		double preco = Double.valueOf(request.getParameter("preco"));
-    		double quantidade = Double.valueOf(request.getParameter("quantidade"));
+            try {
+                request.setCharacterEncoding("UTF-8");
 
-    		Part part = request.getPart("file");
-    		
-    		Produto produto = new Produto(nome, descricao, tipo, preco, quantidade);
-            ServletFileUpload sf = new ServletFileUpload(new DiskFileItemFactory());
-            List<FileItem> multiparts = sf.parseRequest(request);
- 
-                /*Escreve a o arquivo na pasta img*/
-                for (FileItem item : multiparts) {
-                    if (!item.isFormField()) {
-                    	
-                        item.write(new File( request.getServletContext().getRealPath("img")+ File.separator));
-                    }
-                }
-                ProdutoDAO salvarProduto = new ProdutoDAO();
-                boolean salvar = salvarProduto.salvar(produto, multiparts);
-	    		if(salvar) {
-	    			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/cadastrar-produto.jsp");
-	    			
-	       		 	dispatcher.forward(request, response);
-	    		}
- 
+                Produto produto = new Produto(nome, descricao, tipo, preco, quantidade);
+//                List<Part> fileParts = request.getParts().stream().filter(part -> "file".equals(part.getName()) && part.getSize() > 0).collect(Collectors.toList()); // Retrieves <input type="file" name="file" multiple="true">
+//
+//                for (Part filePart : fileParts) {
+//                    String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+//                    InputStream fileContent = filePart.getInputStream();
+//                    // ... (do your job here)
+//                };;
+//                ServletFileUpload upload = new ServletFileUpload(new DiskFileItemFactory());
+//                List<FileItem> multiparts = upload.parseRequest(request); // this is where is kil
+//
+//                /*Escreve a o arquivo na pasta img*/
+//                for (FileItem item : multiparts) {
+//                    if (!item.isFormField()) {
+//                        item.write(new File(request.getServletContext().getRealPath("img") + File.separator));
+//                    }
+//                }
+//                ProdutoDAO salvarProduto = new ProdutoDAO();
+//                boolean salvar = salvarProduto.salvar(produto, multiparts);
+//                if (salvar) {
+//                    RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/cadastrar-produto.jsp");
+//
+//                    dispatcher.forward(request, response);
+//                }
+
                 request.setAttribute("message", "Arquivo carregado com sucesso");
             } catch (Exception ex) {
-                request.setAttribute("message", "Upload de arquivo falhou devido a "+ ex);
+                request.setAttribute("message", "Upload de arquivo falhou devido a " + ex);
             }
- 
+
         } else {
-            request.setAttribute("message","Desculpe este Servlet lida apenas com pedido de upload de arquivos");
+            request.setAttribute("message", "Desculpe este Servlet lida apenas com pedido de upload de arquivos");
         }
- 
+
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/cadastrar-produto.jsp");
-		 	dispatcher.forward(request, response);
+        dispatcher.forward(request, response);
     }
 }
--- projectpiiv/src/main/webapp/WEB-INF/cadastrar-produto.jsp
+++ projectpiiv/src/main/webapp/WEB-INF/cadastrar-produto.jsp
@@ -7,49 +7,49 @@
         <script type="text/javascript" src="https://code.jquery.com/jquery/jquery-3.1.1.min.js"></script>
         <title>Loja de Artigos Esportivos</title>
     </head>
-<body>
-   <form action="${pageContext.request.contextPath}/cadastrar-produto" method="post" enctype="multipart/form-data" novalidate>
-        
+    <body>
+        <form action="${pageContext.request.contextPath}/cadastrar-produto" method="post" enctype="multipart/form-data" novalidate>
 
-    <label for="inputNome">Nome*:</label>
-                    
-                        <input type="text" name="nome" id="nome" placeholder="Digite o nome do Produto" required>
-                    
-                    <br>
-                    <br>
-                    <label for=inputDesc > Descrição*:</label>
-                    <textarea id="descricao" name="descricao"></textarea>
-                    <br>
-                    <br>
-                    <label for="inputTipo">Tipo*:</label>
-                    <select id="tipo" name="tipo">
-                        <option selected>Tenis</option>
-                        <option>Camiseta</option>
-                        <option>Bermudas</option>
-                        <option>Acessórios</option>
-                        <option>Outros</option>
-                    </select>
-                    <br>
-                    <br>
-                    <label for="inputPreco" >Preço*:</label>
-                    <input type="number" name="preco" id="preco" placeholder="9,99" pattern="[0-9]+([,\.][0-9]+)?" min="0" step="any" required>
-                    <br>
-                    <br>
-                    <label for="inputQtd" >Quantidade*:</label>
-                    <input type="number" name="quantidade" id="quantidade" placeholder="9,99" pattern="[0-9]+([,\.][0-9]+)?" min="0" step="any" required>
-                    <br>
-                    <br>
+
+            <label for="inputNome">Nome*:</label>
+
+            <input type="text" name="nome" id="nome" placeholder="Digite o nome do Produto" required>
+
+            <br>
+            <br>
+            <label for=inputDesc > Descrição*:</label>
+            <textarea id="descricao" name="descricao"></textarea>
+            <br>
+            <br>
+            <label for="inputTipo">Tipo*:</label>
+            <select id="tipo" name="tipo">
+                <option selected>Tenis</option>
+                <option>Camiseta</option>
+                <option>Bermudas</option>
+                <option>Acessórios</option>
+                <option>Outros</option>
+            </select>
+            <br>
+            <br>
+            <label for="inputPreco" >Preço*:</label>
+            <input type="number" name="preco" id="preco" placeholder="9,99" pattern="[0-9]+([,\.][0-9]+)?" min="0" step="any" required>
+            <br>
+            <br>
+            <label for="inputQtd" >Quantidade*:</label>
+            <input type="number" name="quantidade" id="quantidade" placeholder="9,99" pattern="[0-9]+([,\.][0-9]+)?" min="0" step="any" required>
+            <br>
+            <br>
+            <br/>
+            <br/>
+
+            <input type="file" name="file" id="file" multiple/> 
+            <button type="submit">Enviar </button>
+        </form>
+        <label>${message}</label>
         <br/>
         <br/>
-
-     <input type="file" name="file" id="file"/> 
-        <button type="submit">Enviar </button>
-    </form>
-    <label>${message}</label>
-    <br/>
-    <br/>
-    <form action="upload" method="get" >
-        <input type="submit" value="Visalizar imagem" />
-    </form>
-</body>
+        <form action="upload" method="get" >
+            <input type="submit" value="Visalizar imagem" />
+        </form>
+    </body>
 </html>
