a522b3bb0fed7c3a95916948042428fa1056814e
marcelo.moraes <>
2020-03-05 08:33:21 -0300

    teste salvar multi imagens em pasta local.

--- projectpiiv/src/main/java/br/senac/sp/grupoum/projectpiiv/controllers/ProdutoServlet.java
+++ projectpiiv/src/main/java/br/senac/sp/grupoum/projectpiiv/controllers/ProdutoServlet.java
@@ -40,7 +40,7 @@ public class ProdutoServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         /*Identifica se o formulario é do tipo multipart/form-data*/
-        MultipartRequest m = new MultipartRequest(request, "C:/Users/gabriel.hsantos21/Desktop/teste");
+        MultipartRequest m = new MultipartRequest(request, "C:\\Users\\marcelo.moraes\\Desktop\\teste");
         String nome = m.getParameter("nome");
         String descricao = m.getParameter("descricao");
         String tipo = m.getParameter("tipo");
