bed594f9309763bfdaba5846c8a6ebfd49097452
Gabriel Holanda <gabriel_ebot@hotmail.com>
2020-03-09 08:47:20 -0300

    Adicionado spring ao projeto

---
+++ projectpiiv-Spring/projectpiiv/src/main/java/com/br/senac/sp/grupoum/projectpiiv/ProjectpiivApplication.java
@@ -0,0 +1,13 @@
+package com.br.senac.sp.grupoum.projectpiiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectpiivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectpiivApplication.class, args);
+	}
+
+}
---
+++ projectpiiv-Spring/projectpiiv/src/test/java/com/br/senac/sp/grupoum/projectpiiv/ProjectpiivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.senac.sp.grupoum.projectpiiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectpiivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
