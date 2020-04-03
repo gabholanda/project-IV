137d542cade299943c415aab481665ed87365d1b
marcelo moraes <>
2020-04-03 14:56:34 -0300

    Criada model user, funcionario, admin e estoquista Autenticador, Criação de usuário ok.

--- projectpiiv/src/main/java/br/senac/sp/grupoum/projectpiiv/controllers/CadastrarUserServlet.java
+++ projectpiiv/src/main/java/br/senac/sp/grupoum/projectpiiv/controllers/CadastrarUserServlet.java
@@ -6,8 +6,10 @@
 package br.senac.sp.grupoum.projectpiiv.controllers;
 
 import br.senac.sp.grupoum.projectpiiv.models.Admin;
+import br.senac.sp.grupoum.projectpiiv.models.AutenticadorEmail;
 import br.senac.sp.grupoum.projectpiiv.models.Estoquista;
 import br.senac.sp.grupoum.projectpiiv.models.Funcionario;
+import br.senac.sp.grupoum.projectpiiv.models.Usuario;
 import dao.FuncionarioDAO;
 import java.io.IOException;
 import java.text.ParseException;
@@ -42,17 +44,44 @@ public class CadastrarUserServlet extends HttpServlet {
             throws ServletException, IOException {
         HttpSession sessao = request.getSession();
         request.setCharacterEncoding("UTF-8");
+        String usuario = request.getParameter("usuario");
         String nome = request.getParameter("nome");
         String email = request.getParameter("email");
         String senha = request.getParameter("senha");
         String tipo = request.getParameter("tipo");
+        String status = request.getParameter("status");
+
         Funcionario funcionario;
         boolean salvou = false;
+
+        int tamanhoNome = nome.length();
+        if (tamanhoNome < 5) {
+            boolean erroNome = false;
+            request.setAttribute("ErroNome", erroNome);
+            request.getRequestDispatcher("/WEB-INF/cadastrar-usuario.jsp")
+                    .forward(request, response);
+        } else {
+
+            AutenticadorEmail autenticar = new AutenticadorEmail();
+            boolean autenticador = autenticar.validadorEmail(email);
+            boolean validar = false;
+            if (status == null) {
+                validar = false;
+            } else {
+                validar = true;
+            }
+            boolean erroEmail = false;
+            request.setAttribute("ErroEmail", erroEmail);
+            request.getRequestDispatcher("/WEB-INF/cadastrar-usuario.jsp")
+                    .forward(request, response);
+
+        }
+
         if (tipo.equals("admin")) {
-            funcionario = new Admin(email, senha, nome);
+            funcionario = new Admin(email, senha, nome, tipo, salvou);
             salvou = FuncionarioDAO.salvar(funcionario);
         } else if (tipo.equals("estoquista")) {
-            funcionario = new Estoquista(email, senha, nome);
+            funcionario = new Estoquista(email, senha, nome, tipo, salvou);
             salvou = FuncionarioDAO.salvar(funcionario);
         }
         if (salvou) {
